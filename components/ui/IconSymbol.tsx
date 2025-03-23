aabefd4817f3b72c167d166260519d6878794f33
강혁준 <rkd35879@gmail.com>
2025-03-23 14:35:45 +0900

    메인화면 수정

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -10,9 +10,8 @@ const MAPPING = {
   // See MaterialIcons here: https://icons.expo.fyi
   // See SF Symbols in the SF Symbols app on Mac.
   'house.fill': 'home',
-  'paperplane.fill': 'send',
-  'chevron.left.forwardslash.chevron.right': 'code',
-  'chevron.right': 'chevron-right',
+  'setting.fill': 'settings',
+  'chatting.fill': 'chat'
 } as Partial<
   Record<
     import('expo-symbols').SymbolViewProps['name'],
